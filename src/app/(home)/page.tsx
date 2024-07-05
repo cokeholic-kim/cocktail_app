1f067bcf0fe89bdc5df0d603799bb3e3405c3167
dong_hy <kimadonghyon99@gmail.com>
2024-07-05 22:54:59 +0900

    feat : 메인화면 1. 메인화면 컴포넌트 2. 카드 컴포넌트 3. 로딩화면

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+import CocktailCard from "./cocktailCard";
 
-export default function Home() {
+export const BASE_URL = "http://localhost:8082"
+
+interface CocktailCardProps {
+  imagePath: string;
+  cocktailName: string;
+  description: string;
+}
+
+async function getCocktail() {
+  return fetch(BASE_URL + "/cocktail/getAll").then((response) =>
+    response.json()
+  );
+}
+
+export default async function Home() {
+  const cocktails = await getCocktail();
   return (
     <div>
-      
+      <div className="flex justify-start flex-wrap">
+        {
+        cocktails.body.map((cocktail:CocktailCardProps,index:number) => {
+          return <CocktailCard 
+            key={index}
+            imagePath={cocktail.imagePath}
+            name={cocktail.cocktailName}
+            description={cocktail.description}
+          />
+        })  
+      }
+      </div>
     </div>
   );
 }

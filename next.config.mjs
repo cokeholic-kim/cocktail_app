dfcb977576cbddad998131af4ccc21976b79e98f
dong_hy <kimadonghyon99@gmail.com>
2024-07-09 21:28:00 +0900

    feat : 세팅 파일

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import withPWA from 'next-pwa';
 
 const nextConfig = {
   // 기존 nextConfig 설정
+  images: {
+    domains: ['s3.ap-northeast-2.amazonaws.com','wallpapers.com'],
+  },
 };
 
 export default withPWA({

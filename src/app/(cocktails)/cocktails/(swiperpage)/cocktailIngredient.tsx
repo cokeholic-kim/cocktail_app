7653f62b56aff9e294ab8e37d8f6435aae2debfc
dong_hy <kimadonghyon99@gmail.com>
2024-09-15 16:37:19 +0900

    refactor : NewIngredientCard 별도 컴포넌트파일로 분리.

--- src/app/(cocktails)/cocktails/(swiperpage)/cocktailIngredient.tsx
+++ src/app/(cocktails)/cocktails/(swiperpage)/cocktailIngredient.tsx
@@ -4,44 +4,13 @@ import { CocktailRequest } from '../AddCocktailSwiper';
 import SimpleIngredientCard from '@/app/(myingredients)/myingredients/SimpleIngredientCard';
 import { IngredientChecked } from '@/app/(myingredients)/myingredients/MyIngredientBody';
 import NewIngredientForm from './(newIngredient)/NewIngredientForm';
+import NewIngredientCard from '@/app/(ingredients)/ingredients/NewIngredientCard';
 
 interface CocktailIngredientProps{
     ingredients:IngredientChecked[],
     cocktailRequest: CocktailRequest,
     setCocktailRequest: React.Dispatch<React.SetStateAction<CocktailRequest>>
 }
-
-
-
-
-function NewIngredientCard({handleClickNewIngredient}:{handleClickNewIngredient:()=>void}){
-  return (
-      <div className="md:w-1/3 w-1/3 p-6 
-      bg-white border border-gray-200 
-      rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-      onClick={handleClickNewIngredient}
-      >
-        <div className="w-full h-44 mb-4 relative">
-          <div className="absolute inset-0 bg-slate-300 flex items-center justify-center">
-            <Image
-              src={"/assets/questionMark.png"}
-              alt={"새로운재료"}
-              width={400}
-              height={300}
-              className="rounded-t-lg w-full h-full object-contain"
-            />
-          </div>
-        </div>
-        <div className="mb-4">
-          <p className="font-normal text-gray-700 dark:text-gray-400 text-lg">
-            {"찾으시는 재료가 없다면 등록해주세요"}
-          </p>
-        </div>
-      </div>
-    );
-}
-
-
 
 function CocktailIngredient({ingredients,cocktailRequest,setCocktailRequest}:CocktailIngredientProps) {
   const [ingredientData, setIngredientData] = useState<IngredientChecked[]>([]);
@@ -109,7 +78,7 @@ function CocktailIngredient({ingredients,cocktailRequest,setCocktailRequest}:Coc
       </>
       }
       <div
-        className="flex justify-start flex-wrap overflow-y-scroll overflow-x-hidden"
+        className="flex justify-start flex-wrap overflow-y-scroll overflow-x-hidden h-full"
         style={{ height: "80vh" }}
       >
         {

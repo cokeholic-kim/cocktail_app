932741038bfae66439de1bc6a53784b23141d62f
dong_hy <kimadonghyon99@gmail.com>
2024-09-15 16:40:55 +0900

    refactor : 나의재료 검색페이지수정 1. 모든재료를가지고있는경우 배열을 앞으로 이동하도록 함수추가.

--- src/app/(myingredients)/myingredients/fitCocktail/Ingredients.tsx
+++ src/app/(myingredients)/myingredients/fitCocktail/Ingredients.tsx
@@ -5,6 +5,7 @@ import { BASE_URL } from '@/app/(common)/common';
 import { CocktailFit } from '@/app/(common)/commonProps';
 import FitCocktailCard from './fitCocktailCard';
 
+
 function Ingredients() {
     const param = useSearchParams();
     const data = param.get("checkedIngredients")
@@ -12,11 +13,30 @@ function Ingredients() {
 
     const [cocktail, setCocktail] = useState<CocktailFit[]>([]);
 
+    const sendIngredientsToAPI = async (ingredientNames: string[]) => {
+      const jsondata = {myIngredient : ingredientNames}
+      try {
+        const response = await fetch(`${BASE_URL}/ingredient/getFitCocktailList`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(jsondata),
+        });
+    
+        const data = await response.json();
+        return data.body;
+      } catch (error) {
+        console.error('Error sending ingredients to API:', error);
+        throw error;
+      }
+    };
+
     useEffect(()=>{
         const fetchData = async () => {
             try {
               const fetchData = await sendIngredientsToAPI(ingredients);
-              setCocktail(fetchData);
+              setCocktail(moveCocktailWithOutExcludeIngredient(fetchData));
             } catch (error) {
               console.error('Error fetching data:', error);
             }
@@ -24,25 +44,21 @@ function Ingredients() {
           fetchData()
     },[])
 
-    const sendIngredientsToAPI = async (ingredientNames: string[]) => {
-        const jsondata = {myIngredient : ingredientNames}
-        const formData = new FormData();
-        try {
-          const response = await fetch(`${BASE_URL}/ingredient/getFitCocktailList`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(jsondata),
-          });
-      
-          const data = await response.json();
-          return data.body;
-        } catch (error) {
-          console.error('Error sending ingredients to API:', error);
-          throw error;
+    //모든재료를 가지고있는 칵테일을 가장앞으로 보내주는 함수
+    const moveCocktailWithOutExcludeIngredient = (cocktails:CocktailFit[]) => {
+      const withExclude:CocktailFit[] = [];
+      const withoutExclude:CocktailFit[] = [];
+
+      cocktails.forEach(cocktail => {
+        if (cocktail.excludeIngredient && cocktail.excludeIngredient.length > 0) {
+          withExclude.push(cocktail);
+        } else {
+          withoutExclude.push(cocktail);
         }
-      };
+      });
+
+      return [...withoutExclude, ...withExclude];
+    }
 
   return (
     <div className="flex justify-start flex-wrap">
